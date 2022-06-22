2002d66859f5b6676f32203a49dda74f5105f2ce
licosan <licosan@reec.be>
2022-06-22 16:36:07 +0200

    let default route run before testing, to keep one single environment

--- tests/router_test.py
+++ tests/router_test.py
@@ -2,11 +2,11 @@ from selenium.webdriver.support.ui import WebDriverWait
 
 baseurl = 'https://eismea.internike.com'
 
-def check_route_ready(browser):
-    return browser.execute_script('return app.Router.route_ready;')
+def check_routeReady(browser):
+    return browser.execute_script('return app.Router.routeReady;')
 
 def wait_for_router_ready(browser):
-    WebDriverWait(browser, 10).until(lambda browser: browser.execute_script('return app.Router.route_ready;'))
+    WebDriverWait(browser, 10).until(lambda browser: browser.execute_script('return app.Router.routeReady;'))
 
 def launch_router(browser, role):
     def chrl(browser):
@@ -33,7 +33,8 @@ test_serie = [
     {   'name'      : 'defaultroute1',
         'desc'      : """Trying non-existing route to defaultRoute""",
         'url'       : baseurl + '/doesnotexist',
-        'todos'     : [ {'function': launch_router, 'args': ['']},
+        'todos'     : [ {'function': wait_for_router_ready, 'args': []}, ##Let the normal route settle, then artificially trigger our test route
+                        {'function': launch_router, 'args': ['']},
                         {'function': wait_for_router_ready, 'args': []} ],
         'check'     : lambda browser: 'Doh, this app does not serve this URL !' in browser.find_element_by_css_selector('body').text
                                   and 'Error: No route found for this URL' in browser.find_element_by_css_selector('body').text,
@@ -42,7 +43,8 @@ test_serie = [
     {   'name'      : 'defaultroute2',
         'desc'      : """Trying if "/" is routed to "defaultRoute.modulesChoice" """,
         'url'       : baseurl + '',
-        'todos'     : [ {'function': launch_router, 'args': ['']},
+        'todos'     : [ {'function': wait_for_router_ready, 'args': []},
+                        {'function': launch_router, 'args': ['']},
                         {'function': wait_for_router_ready, 'args': []} ],
         'check'     : lambda browser: 'Choose your module' in browser.find_element_by_css_selector('body').text,
         'debuginfo' : lambda browser: 'Content: %s\n' %browser.find_element_by_css_selector('body').text,
@@ -50,7 +52,8 @@ test_serie = [
     {   'name'      : 'defaultroute3',
         'desc'      : """Trying if "/" is routed to "defaultRoute.ExpertmodulesChoice" for role expert """,
         'url'       : baseurl + '',
-        'todos'     : [ {'function': launch_router, 'args': ['expert']},
+        'todos'     : [ {'function': wait_for_router_ready, 'args': []},
+                        {'function': launch_router, 'args': ['expert']},
                         {'function': wait_for_router_ready, 'args': []} ],
         'check'     : lambda browser: 'Choose your expert module' in browser.find_element_by_css_selector('body').text,
         'debuginfo' : lambda browser: 'Content: %s\n' %browser.find_element_by_css_selector('body').text,
@@ -58,7 +61,8 @@ test_serie = [
     {   'name'      : 'home',
         'desc'      : """Trying if "/app1" is routed app1.app1MainCtrl, method forced to "home" by config""",
         'url'       : baseurl + '/app1',
-        'todos'     : [ {'function': launch_router, 'args': ['']},
+        'todos'     : [ {'function': wait_for_router_ready, 'args': []},
+                        {'function': launch_router, 'args': ['']},
                         {'function': wait_for_router_ready, 'args': []} ],
         'check'     : lambda browser: 'Called home' in browser.find_element_by_css_selector('body').text,
         'debuginfo' : lambda browser: 'Content: %s\n' %browser.find_element_by_css_selector('body').text,
@@ -67,7 +71,8 @@ test_serie = [
         'desc'      : """Trying if "/app1/helloWorld" is routed app1.app1MainCtrl, 
                          method "helloWorld" taken from URL""",
         'url'       : baseurl + '/app1/helloWorld',
-        'todos'     : [ {'function': launch_router, 'args': ['']},
+        'todos'     : [ {'function': wait_for_router_ready, 'args': []},
+                        {'function': launch_router, 'args': ['']},
                         {'function': wait_for_router_ready, 'args': []} ],
         'check'     : lambda browser: 'Called helloWorld' in browser.find_element_by_css_selector('body').text,
         'debuginfo' : lambda browser: 'Content: %s\n' %browser.find_element_by_css_selector('body').text,
@@ -76,7 +81,8 @@ test_serie = [
         'desc'      : """Trying if "/app1/badmethod" is routed to defaultRoute,
                          with "Could not find method" error""",
         'url'       : baseurl + '/app1/badmethod',
-        'todos'     : [ {'function': launch_router, 'args': ['']},
+        'todos'     : [ {'function': wait_for_router_ready, 'args': []},
+                        {'function': launch_router, 'args': ['']},
                         {'function': wait_for_router_ready, 'args': []} ],
         'check'     : lambda browser: ('Doh, this app does not serve this URL !' in browser.find_element_by_css_selector('body').text) 
                                   and ('Error: Could not find method' in browser.find_element_by_css_selector('body').text),
@@ -86,7 +92,8 @@ test_serie = [
         'desc'      : """Trying if "/app1/user/123X" as "suckeleer" is routed to UserCtrl, method "userDetails",
                          dynamic param uid=123X, no param validation""",
         'url'       : baseurl + '/app1/user/123X',
-        'todos'     : [ {'function': launch_router, 'args': ['suckeleer']},
+        'todos'     : [ {'function': wait_for_router_ready, 'args': []},
+                        {'function': launch_router, 'args': ['suckeleer']},
                         {'function': wait_for_router_ready, 'args': []} 
                       ],
         'check'     : lambda browser: ('Called userDetails' in browser.find_element_by_css_selector('body').text) 
@@ -97,7 +104,8 @@ test_serie = [
         'desc'      : """Trying if "/app1/user/123X" as "dikkenek" is routed to UserCtrl, method "userDetails", 
                          dynamic param uid=123X + fixed param, no param validation""",
         'url'       : baseurl + '/app1/user/123X',
-        'todos'     : [ {'function': launch_router, 'args': ['dikkenek']},
+        'todos'     : [ {'function': wait_for_router_ready, 'args': []},
+                        {'function': launch_router, 'args': ['dikkenek']},
                         {'function': wait_for_router_ready, 'args': []},                          
                       ],
         'check'     : lambda browser: ('Called userDetails' in browser.find_element_by_css_selector('body').text) 
@@ -109,7 +117,8 @@ test_serie = [
         'desc'      : """Trying if "/app1/user/123X" as "denbaas" fails on param validation, and therefore ends up in 
                          app1MainCtrl looking for method user, which does not exist => default page""",
         'url'       : baseurl + '/app1/user/123X',
-        'todos'     : [ {'function': launch_router, 'args': ['denbaas']},
+        'todos'     : [ {'function': wait_for_router_ready, 'args': []},
+                        {'function': launch_router, 'args': ['denbaas']},
                         {'function': wait_for_router_ready, 'args': []},                          
                       ],
         'check'     : lambda browser: ('Doh, this app does not serve this URL !' in browser.find_element_by_css_selector('body').text) 
@@ -120,7 +129,8 @@ test_serie = [
         'desc'      : """Trying if "/app2/" is routed to app2MainCtrl,
                          with method defaulting to index""",
         'url'       : baseurl + '/app2/',
-        'todos'     : [ {'function': launch_router, 'args': ['']},
+        'todos'     : [ {'function': wait_for_router_ready, 'args': []},
+                        {'function': launch_router, 'args': ['']},
                         {'function': wait_for_router_ready, 'args': []} ],
         'check'     : lambda browser: ('Called index' in browser.find_element_by_css_selector('body').text),
         'debuginfo' : lambda browser: 'Content: %s\n' %browser.find_element_by_css_selector('body').text,
@@ -129,7 +139,8 @@ test_serie = [
         'desc'      : """Trying if "/app2/proposals" is routed to for user not po or paco is routed to app2MainCtrl, 
                          method proposals which should not be found ! => default route""",
         'url'       : baseurl + '/app2/proposals',
-        'todos'     : [ {'function': launch_router, 'args': ['']},
+        'todos'     : [ {'function': wait_for_router_ready, 'args': []},
+                        {'function': launch_router, 'args': ['']},
                         {'function': wait_for_router_ready, 'args': []} ],
         'check'     : lambda browser: ('Doh, this app does not serve this URL !' in browser.find_element_by_css_selector('body').text)
                                   and ('Doh, this app does not serve this URL !' in browser.find_element_by_css_selector('body').text),
@@ -138,7 +149,8 @@ test_serie = [
     {   'name'      : 'role2',
         'desc'      : """Trying if "/app2/proposals" is routed to app2proposalsCtrl, method list""",
         'url'       : baseurl + '/app2/proposals',
-        'todos'     : [ {'function': launch_router, 'args': ['paco']},
+        'todos'     : [ {'function': wait_for_router_ready, 'args': []},
+                        {'function': launch_router, 'args': ['paco']},
                         {'function': wait_for_router_ready, 'args': []} ],
         'check'     : lambda browser: ('Called list' in browser.find_element_by_css_selector('body').text),
         'debuginfo' : lambda browser: 'Content: %s\n' %browser.find_element_by_css_selector('body').text,
@@ -146,7 +158,8 @@ test_serie = [
     {   'name'      : 'split_params',
         'desc'      : """Trying if "/app2/proposal/987/reviews/456" takes separated parameters properly""",
         'url'       : baseurl + '/app2/proposal/987/reviews/456',
-        'todos'     : [ {'function': launch_router, 'args': ['paco']},
+        'todos'     : [ {'function': wait_for_router_ready, 'args': []},
+                        {'function': launch_router, 'args': ['paco']},
                         {'function': wait_for_router_ready, 'args': []} ],
         'check'     : lambda browser: ('Called getOneReview' in browser.find_element_by_css_selector('body').text)
                                   and ( '{"pid":"987","rid":"456"}' in browser.find_element_by_css_selector('body').text),
@@ -155,7 +168,8 @@ test_serie = [
     {   'name'      : 'dependency_helper',
         'desc'      : """Trying if "/app1/user/123X" loads dependency class "userHelper" """,
         'url'       : baseurl + '/app1/user/123X',
-        'todos'     : [ {'function': launch_router, 'args': ['dikkenek']},
+        'todos'     : [ {'function': wait_for_router_ready, 'args': []},
+                        {'function': launch_router, 'args': ['dikkenek']},
                         {'function': wait_for_router_ready, 'args': []},
                       ],
         'check'     : lambda browser: check_for_class(browser, 'userHelper'),
@@ -164,7 +178,8 @@ test_serie = [
     {   'name'      : 'makelink1',
         'desc'      : """Trying if makelink('app1/app1MainCtrl','home',{}) gives "/app1"  """,
         'url'       : baseurl + '/',
-        'todos'     : [ {'function': launch_router, 'args': ['toto']},
+        'todos'     : [ {'function': wait_for_router_ready, 'args': []},
+                        {'function': launch_router, 'args': ['toto']},
                         {'function': wait_for_router_ready, 'args': []},
                       ],
         'check'     : lambda browser: router_makelink(browser, 'app1/app1MainCtrl','home','{}') == '/app1',
@@ -173,7 +188,8 @@ test_serie = [
     {   'name'      : 'makelink2',
         'desc'      : """Trying if makelink('/common/User/UserCtrl','userDetails',{'uid':'789'}) gives "/app1/user/789"  """,
         'url'       : baseurl + '/',
-        'todos'     : [ {'function': launch_router, 'args': ['dikkenek']},
+        'todos'     : [ {'function': wait_for_router_ready, 'args': []},
+                        {'function': launch_router, 'args': ['dikkenek']},
                         {'function': wait_for_router_ready, 'args': []},
                       ],
         'check'     : lambda browser: router_makelink(browser, '/common/User/UserCtrl','userDetails',"""{'uid':'789'}""") == '/app1/user/789',
@@ -182,7 +198,8 @@ test_serie = [
     {   'name'      : 'external',
         'desc'      : """Trying if "/extapp/proposal/999/reviews/888" reloads the page to external URL with parameters """,
         'url'       : baseurl + '/extapp/proposal/999/reviews/888',
-        'todos'     : [ {'function': launch_router, 'args': ['expert']},
+        'todos'     : [ {'function': wait_for_router_ready, 'args': []},
+                        {'function': launch_router, 'args': ['expert']},
                         {'function': wait_for_correct_current_url, 'args': ['https://www.internike.com/printenv.php?pid=999&rid=888']},
                       ],
         'check'     : lambda browser: browser.current_url == 'https://www.internike.com/printenv.php?pid=999&rid=888',
